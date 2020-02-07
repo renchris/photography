9b0a3088b19243653ffb3b5fb3499b29a3f72530
Chris <u4y0b@ugrad.cs.ubc.ca>
2020-02-06 22:26:39 -0800

    updated index.js homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+const IndexPage = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Welcome to my World!</h1>
+            <h2>Hello, I'm Chris, a software developer and photographer here in Vancouver BC</h2>
+            <Footer />
+            </div>
+    )
+}
+
+export default IndexPage
